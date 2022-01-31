b5101c3cf58f68368c3f03d270a950e451527743
GrowLLateral <69913510+gutierrezmiguel@users.noreply.github.com>
2022-01-30 21:46:29 -0500

    add cors

--- ovaApi/src/main/java/com/pwa/ovaApi/OvaApiApplication.java
+++ ovaApi/src/main/java/com/pwa/ovaApi/OvaApiApplication.java
@@ -20,7 +20,7 @@ public class OvaApiApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://localhost:3000","https://inventario-tienda-la-mejor.herokuapp.com"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://localhost:3000","https://inventario-tienda-la-mejor.herokuapp.com","http://localhost:8080"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
